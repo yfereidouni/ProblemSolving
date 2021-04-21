8bf67c3cad7f3d71471ad540e21e1789ef2a30e0
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-04-21 15:16:10 +0430

    Done

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[] { 1, 2, 3, 4, 10, 11 }; //Expected Result: 31
+            Console.WriteLine(myfunc(ar));
+            Console.ReadLine();
+        }
+        private static int myfunc(int[] ar)
+        {
+            int sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
